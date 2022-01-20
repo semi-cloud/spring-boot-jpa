bcec2cf8268c73cb0ad78b6155a85b010b66301c
semi_cloud <sksmsrkdtpal@naver.com>
2022-01-20 16:11:23 +0900

    querydsl 설정과 검증

---
+++ querydsl/src/test/java/JPA/querydsl/QuerydslApplicationTests.java
@@ -0,0 +1,40 @@
+package JPA.querydsl;
+
+import JPA.querydsl.entity.Hello;
+import JPA.querydsl.entity.QHello;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+@Transactional
+class QuerydslApplicationTests {
+
+	//@PersistenceContext   java 표준 스펙
+	@Autowired
+	EntityManager em;
+
+	@Test
+	void contextLoads() {
+		Hello hello = new Hello();
+		em.persist(hello);
+
+		JPAQueryFactory query = new JPAQueryFactory(em);
+		QHello qHello = new QHello("h");
+
+		Hello result = query
+				.selectFrom(qHello)
+				.fetchOne();
+
+		assertThat(result).isEqualTo(hello);
+		assertThat(result.getId()).isEqualTo(hello.getId());
+	}
+
+}
