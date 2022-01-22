fcf10c9b9bd60f888b4c9eec45d36c241e18fd05
semi_cloud <sksmsrkdtpal@naver.com>
2022-01-22 16:05:57 +0900

    프로파일 설정과 데이터 초기화

---
+++ querydsl/src/main/java/JPA/querydsl/controller/InitMember.java
@@ -0,0 +1,41 @@
+package JPA.querydsl.controller;
+
+import JPA.querydsl.entity.Member;
+import JPA.querydsl.entity.Team;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.annotation.PostConstruct;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+@Profile("local")       //test 에 영향 주지 X
+@Component
+@RequiredArgsConstructor
+public class InitMember {
+
+    private final InitMemberService initMemberService;
+
+    @PostConstruct
+    public void init(){
+        initMemberService.init();
+    }
+
+    static class InitMemberService{
+        @PersistenceContext
+        EntityManager em;
+
+        @Transactional
+        public void init(){
+            Team teamA = new Team("teamA");
+            Team teamB = new Team("teamB");
+
+            for (int i = 0; i < 100; i++) {
+                Team selectedTeam = i % 2 == 0 ? teamA : teamB;
+                em.persist(new Member("member" + i, i, selectedTeam));
+            }
+        }
+    }
+}
