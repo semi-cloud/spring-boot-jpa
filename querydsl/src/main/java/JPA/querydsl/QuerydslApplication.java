bef7dade7b740ca5cd527bc6a102fff6b9e10eca
semi_cloud <sksmsrkdtpal@naver.com>
2022-01-22 15:17:31 +0900

    순수 JPA 레파지토리, querydsl

--- querydsl/src/main/java/JPA/querydsl/QuerydslApplication.java
+++ querydsl/src/main/java/JPA/querydsl/QuerydslApplication.java
@@ -1,13 +1,23 @@
 package JPA.querydsl;
 
+import com.querydsl.jpa.impl.JPAQueryFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import javax.persistence.EntityManager;
 
 @SpringBootApplication
 public class QuerydslApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(QuerydslApplication.class, args);
+	}
+
+	//스프링 빈으로 등록
+	@Bean
+	JPAQueryFactory jpaQueryFactory(EntityManager em){
+		return new JPAQueryFactory(em);
 	}
 
 }
