3ba89d4d64675515418b3e3e54cd17b82ae57f0e
semi_cloud <sksmsrkdtpal@naver.com>
2022-01-22 15:46:57 +0900

    동적 쿼리와 성능 최적화 조회

--- querydsl/src/main/java/JPA/querydsl/repository/MemberJpaRepository.java
+++ querydsl/src/main/java/JPA/querydsl/repository/MemberJpaRepository.java
@@ -1,16 +1,27 @@
 package JPA.querydsl.repository;
 
+import JPA.querydsl.dto.MemberSearchCondition;
+import JPA.querydsl.dto.MemberTeamDto;
+import JPA.querydsl.dto.QMemberDto;
+import JPA.querydsl.dto.QMemberTeamDto;
 import JPA.querydsl.entity.Member;
 import JPA.querydsl.entity.QMember;
+import JPA.querydsl.entity.QTeam;
+import com.querydsl.core.BooleanBuilder;
+import com.querydsl.core.types.Predicate;
+import com.querydsl.core.types.dsl.BooleanExpression;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
+import org.springframework.util.StringUtils;
 
 import javax.persistence.EntityManager;
 import java.util.List;
 import java.util.Optional;
 
 import static JPA.querydsl.entity.QMember.member;
+import static JPA.querydsl.entity.QTeam.team;
+import static org.springframework.util.StringUtils.*;
 
 @Repository
 @RequiredArgsConstructor
@@ -51,4 +62,74 @@ public class MemberJpaRepository {
                 .where(member.username.eq(username))   //자동 파라미터 바인딩
                 .fetch();
     }
+
+    public List<MemberTeamDto> searchByBuilder(MemberSearchCondition condition){
+
+        BooleanBuilder builder = new BooleanBuilder();
+        //null or ""
+        if (hasText(condition.getUsername())) {
+            builder.and(member.username.eq(condition.getUsername()));
+        }
+        if (hasText(condition.getTeamName())) {
+            builder.and(team.name.eq(condition.getTeamName()));
+        }
+        if (condition.getAgeGoe() != null) {
+            builder.and(member.age.goe(condition.getAgeGoe()));
+        }
+        if (condition.getAgeLoe() != null) {
+            builder.and(member.age.loe(condition.getAgeLoe()));
+        }
+
+        return queryFactory
+                .select(new QMemberTeamDto(
+                        member.id.as("memberId"),
+                        member.username,
+                        member.age,
+                        team.id.as("teamId"),
+                        team.name.as("teamName")
+                ))
+                .from(member)
+                .leftJoin(member.team, team)
+                .where(builder)
+                .fetch();
+    }
+
+    public List<MemberTeamDto> search(MemberSearchCondition condition) {
+        return queryFactory
+                .select(new QMemberTeamDto(
+                        member.id.as("memberId"),
+                        member.username,
+                        member.age,
+                        team.id.as("teamId"),
+                        team.name.as("teamName")
+                ))
+                .from(member)
+                .leftJoin(member.team, team)
+                .where(
+                        usernameEq(condition.getUsername()),
+                        teamNameEq(condition.getTeamName()),
+                        ageGoe(condition.getAgeGoe()),
+                        ageLoe(condition.getAgeLoe()))
+                .fetch();
+    }
+
+
+    private BooleanExpression usernameEq(String username) {
+        return hasText(username)? member.username.eq(username) : null;
+    }
+
+    private BooleanExpression teamNameEq(String teamName) {
+        return hasText(teamName)? team.name.eq(teamName) : null;
+    }
+
+    private BooleanExpression ageGoe(Integer ageGoe) {
+        return ageGoe != null ? member.age.goe(ageGoe) : null;
+    }
+
+    private BooleanExpression ageLoe(Integer ageLoe) {
+        return ageLoe != null ? member.age.loe(ageLoe) : null;
+    }
+
+
+
 }
